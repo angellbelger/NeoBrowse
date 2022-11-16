9a11d3fd2b2a7e72999dd381a0bcf173aa5883d8
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-11-16 15:45:01 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-time = window.document.querySelector('')
+var date = new Date()
+var hour = date.getHour()
+var minutes = date.getMinutes()
+
+var display = window.document.querySelector('p#time')
+
+function clock(paramHour=0, paramMinutes=00){
+    display.innerHTML = `${hour}:${minutes}`
+}
