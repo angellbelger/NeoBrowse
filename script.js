f954d494af74cbbe392da2d236e0fa87ed3191c1
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-11-16 15:51:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var date = new Date()
 var hour = date.getHour()
 var minutes = date.getMinutes()
 
+
 var display = window.document.querySelector('p#time')
 
-function clock(paramHour=0, paramMinutes=00){
+function clock(paramHour = 0, paramMinutes = 0){
     display.innerHTML = `${hour}:${minutes}`
 }
